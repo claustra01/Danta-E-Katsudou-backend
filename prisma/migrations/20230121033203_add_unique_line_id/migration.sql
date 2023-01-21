672d93cc682da6e762b411729ce355cd31146541
sny430 <shinya430.g@gmail.com>
2023-01-22 00:43:06 +0900

    :construction: remove all file

--- prisma/migrations/20230121033203_add_unique_line_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[lineID]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "User_lineID_key" ON "User"("lineID");
